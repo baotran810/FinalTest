7fa1625606b8bcd5de6890c84d40a103713586c7
baotran810 <44739726+baotran810@users.noreply.github.com>
2019-12-20 08:52:20 +0700

    TC_JOOMLA_ARTICLE_002

--- src/test/java/joomla/page/ArticlePage.java
+++ src/test/java/joomla/page/ArticlePage.java
@@ -8,12 +8,18 @@ public class ArticlePage {
 	
 	private By btnNew = By
 			.xpath("//div[@id='toolbar']//div[@id='toolbar-new']");
-	private By confirmMsg = By
-			.xpath("//div[@id='system-message-container']//div[@class='alert-message']");
+	private String checkBox = "//a[normalize-space(text())='%s']/../../preceding-sibling::td//input[@type='checkbox']";
+	private By btnEdit = By.id("toolbar-edit");
 	
-	public AddNewArticle clickNewbutton(){
+	public AddNewArticle clickNewButton(){
 		Constant.WEBDRIVER.findElement(btnNew).click();
 		return new AddNewArticle();
+	}
+	
+	public EditArticlePage clickEditButton(String articleName){
+		Constant.WEBDRIVER.findElement(By.xpath(String.format(checkBox, articleName))).click();
+		Constant.WEBDRIVER.findElement(btnEdit).click();
+		return new EditArticlePage();
 	}
 
 }
---
+++ src/test/java/joomla/page/EditArticlePage.java
@@ -0,0 +1,32 @@
+package joomla.page;
+
+import joomla.constant.Constant;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import com.sun.org.apache.bcel.internal.generic.NEWARRAY;
+
+public class EditArticlePage {
+
+	private By txtTitle = By.id("jform_title");
+	private By iframe = By.id("jform_articletext_ifr");
+	private By txtArea = By.id("tinymce");
+	private By category = By
+			.xpath("//label[@id='jform_catid-lbl']/..//following-sibling::div[@class='controls']");
+	private String categoryItem = "//label[@id='jform_catid-lbl']/..//following-sibling::div//li[text()='%s']";
+	private By btnSaveClose = By
+			.xpath("//div[@id='toolbar']//button[@class='btn btn-small button-save']");
+
+	public void editArticle(String newTitle, String newContent, String newCategory) {
+		WebElement Element5 = Constant.WEBDRIVER.findElement(iframe);
+		Constant.WEBDRIVER.findElement(txtTitle).sendKeys(newTitle);
+		Constant.WEBDRIVER.switchTo().frame(Element5);
+		Constant.WEBDRIVER.findElement(txtArea).sendKeys(newContent);
+		Constant.WEBDRIVER.switchTo().defaultContent();
+		Constant.WEBDRIVER.findElement(category).click();
+		Constant.WEBDRIVER.findElement(By.xpath(String.format(categoryItem, newCategory))).click();
+		Constant.WEBDRIVER.findElement(btnSaveClose).click();
+	}
+
+}
