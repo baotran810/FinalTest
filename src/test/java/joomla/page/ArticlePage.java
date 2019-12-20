da64ee051e0eef33348359e5561234147b19b9ac
baotran810 <44739726+baotran810@users.noreply.github.com>
2019-12-20 11:01:36 +0700

    finish joomla testcase 005

--- src/test/java/joomla/page/ArticlePage.java
+++ src/test/java/joomla/page/ArticlePage.java
@@ -1,9 +1,13 @@
 package joomla.page;
 
+import java.util.concurrent.TimeUnit;
+
 import joomla.constant.Constant;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class ArticlePage {
 
@@ -12,11 +16,14 @@ public class ArticlePage {
 	private String checkboxXpath = "//table[@id='articleList']//tr//a[normalize-space(text())='%s']//ancestor::tr//input[@type='checkbox']";
 	private String btnXpath = "//div[@id='toolbar']//div[@id='toolbar-%s']/button";
 	private String statusXpath = "//table[@id='articleList']//tr//a[normalize-space(text())='%s']//ancestor::tr//span[@class='icon-%s']";
-	private By btnSearchTool = By.cssSelector("button[class$='js-stools-btn-filter']");
-	private By divFilter = By.cssSelector("div[class^='js-stools-container-filters']");
-	private By listStatus = By.xpath("//select[@id='filter_published']/..//span[text()='- Select Status -']");
+	private By btnSearchTool = By
+			.cssSelector("button[class$='js-stools-btn-filter']");
+	private By divFilter = By
+			.cssSelector("div[class^='js-stools-container-filters']");
+	private By listStatus = By
+			.xpath("//select[@id='filter_published']/..//span[text()='- Select Status -']");
 	private String itemStatus = "//select[@id='filter_published']/..//li[text()='%s']";
-	
+
 	private WebElement getCheckbox(String articleName) {
 		return Constant.WEBDRIVER.findElement(By.xpath(String.format(
 				checkboxXpath, articleName)));
@@ -51,7 +58,22 @@ public class ArticlePage {
 				.size() == 1;
 		return exists;
 	}
-	
-	
+
+	public void clickBtnSearchTool() {
+		Constant.WEBDRIVER.findElement(btnSearchTool).click();
+	}
+
+	public void waitForDivFilter(int seconds) {
+		WebDriverWait wait = new WebDriverWait(Constant.WEBDRIVER, seconds);
+		wait.until(ExpectedConditions.visibilityOfElementLocated(divFilter));
+	}
+
+	public void selectStatusDropdownList(String status) {
+		Constant.WEBDRIVER.findElement(listStatus).click();
+		Constant.WEBDRIVER.manage().timeouts()
+				.implicitlyWait(60, TimeUnit.SECONDS);
+		Constant.WEBDRIVER.findElement(
+				By.xpath(String.format(itemStatus, status))).click();
+	}
 
 }
