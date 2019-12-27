454ac02e27b17e147cd54e3d3c61b87a47b31888
baotran810 <44739726+baotran810@users.noreply.github.com>
2019-12-27 10:57:45 +0700

    - Push ArticleHelpPage and ContactHelpPage

--- src/test/java/joomla/page/ContactHelpPage.java
+++ src/test/java/joomla/page/ContactHelpPage.java
@@ -4,26 +4,28 @@ import java.util.Set;
 
 import joomla.constant.Constant;
 
-public class HelpPageOfContact {
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class ContactHelpPage {
 
 	public boolean doesHelpPageDisplays() throws InterruptedException {
-		boolean exist = true;
+		WebDriverWait wait = new WebDriverWait(Constant.WEBDRIVER, 20);
 		String parentWithHandle = Constant.WEBDRIVER.getWindowHandle();
 		Set<String> winHandles = Constant.WEBDRIVER.getWindowHandles();
 		for (String handles : winHandles) {
 			if (!handles.equals(parentWithHandle)) {
 				Constant.WEBDRIVER.switchTo().window(handles);
-				Thread.sleep(1000);
+				wait.until(ExpectedConditions.titleIs(Constant.HELPAGE_TITLE));
 				String title = Constant.WEBDRIVER.getTitle();
-				System.out.println("Title is: " + title);
-				if (title.equals("Joomla! Help Screens"))
-					exist = true;
+				if (title.equals(Constant.HELPAGE_TITLE))
+					return true;
 				else
-					exist = false;
+					return false;
 			}
 		}
 
-		return exist;
+		return false;
 	}
 
 }
